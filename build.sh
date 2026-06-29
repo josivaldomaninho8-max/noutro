77e8677043cee2021889d2fe8f452676bf038094
josivaldomaninho8-max <josivaldomaninho8@gmail.com>
2026-06-29 06:05:47 -1200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update -y
-apt-get install -y wget unzip xvfb libxi6 libgconf-2-4
+apt-get install -y wget unzip xvfb libxi6 libgconf-2-4 jq
 
 # Instalar Chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
@@ -8,9 +8,10 @@ echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /e
 apt-get update -y
 apt-get install -y google-chrome-stable
 
-# Instalar ChromeDriver
+# Instalar ChromeDriver compatível
 CHROME_VERSION=$(google-chrome --version | awk '{print $3}')
-wget -O chromedriver.zip "https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/$(curl -s https://googlechromelabs.github.io/chrome-for-testing/known-good-versions.json | jq -r '.versions[] | select(.version | startswith("'$CHROME_VERSION'")) | .downloads.chrome[0].url' | head -1 | cut -d'/' -f5)/linux64/chromedriver-linux64.zip"
+CHROME_MAJOR=$(echo $CHROME_VERSION | cut -d. -f1)
+wget -O chromedriver.zip "https://storage.googleapis.com/chrome-for-testing-public/${CHROME_VERSION}/linux64/chromedriver-linux64.zip"
 unzip chromedriver.zip
 mv chromedriver-linux64/chromedriver /usr/local/bin/
 chmod +x /usr/local/bin/chromedriver
